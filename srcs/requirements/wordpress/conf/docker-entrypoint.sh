55c1a16d4104cc66c0579cac9f97c285424dc47d
welove-t <tlove.tky@gmail.com>
2023-10-17 17:50:09 +0900

    wordpressのスクリプトを修正

--- srcs/requirements/wordpress/conf/docker-entrypoint.sh
+++ srcs/requirements/wordpress/conf/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e # エラーが発生した場合、スクリプトを停止
 
 # 必要な変数を読み込む
 : "${WORDPRESS_DB_HOST:?Need to set WORDPRESS_DB_HOST}"
@@ -7,10 +8,14 @@
 : "${WORDPRESS_DB_NAME:=wordpress}" # Default to 'wordpress' if not set
 
 # WordPressの設定ファイルを生成
-wp --allow-root core config --dbname="${WORDPRESS_DB_NAME}" --dbuser="${WORDPRESS_DB_USER}" --dbpass="${WORDPRESS_DB_PASSWORD}" --dbhost="${WORDPRESS_DB_HOST}"
+if [ ! -f wp-config.php ]; then
+    wp --allow-root core config --dbname="${WORDPRESS_DB_NAME}" --dbuser="${WORDPRESS_DB_USER}" --dbpass="${WORDPRESS_DB_PASSWORD}" --dbhost="${WORDPRESS_DB_HOST}"
+fi
 
-# WordPressのインストール
-wp --allow-root core install --url="localhost" --title="Your WordPress Site" --admin_user="admin" --admin_password="password" --admin_email="admin@example.com"
+# WordPressが未インストールの場合、インストールする
+if ! $(wp --allow-root core is-installed); then
+    wp --allow-root core install --url="localhost" --title="Your WordPress Site" --admin_user="admin" --admin_password="password" --admin_email="admin@example.com"
+fi
 
 # PHP-FPMを起動
 exec php-fpm7.4 -F
