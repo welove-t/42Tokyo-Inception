f9fc9ac15760c339047c33711babd649636e1aed
welove-t <tlove.tky@gmail.com>
2023-10-23 09:36:31 +0900

    localhostでアクセスできるように設定

--- srcs/requirements/nginx/tools/docker-entrypoint.sh
+++ srcs/requirements/nginx/tools/docker-entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+
 # Replace the DOMAIN_NAME placeholder
 sed -i "s/\${DOMAIN_NAME}/${DOMAIN_NAME}/g" /etc/nginx/sites-available/default
+
+# Change ownership of /var/www/html to www-data, if it's owned by root
+# This is necessary because mounted volumes will inherit the ownership from the host
+# and they are often owned by root.
+if [ "$(stat -c '%U' /var/www/html)" = "root" ]; then
+    chown -R www-data:www-data /var/www/html
+fi
 
 # Start Nginx
 nginx -g 'daemon off;'
